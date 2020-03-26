848963e70e172a504830a4fa9e485a7c9d1f3134
Lidia Sánchez Mérida <lidiasm96@correo.ugr.es>
2020-03-26 11:56:00 +0100

    Añadida la compresión/descompresión de los modelos

--- src/prediccion.py
+++ src/prediccion.py
@@ -14,6 +14,7 @@ import pickle
 import os.path
 from datetime import datetime
 import numpy
+import zipfile
 
 class Prediccion:
     
@@ -37,25 +38,43 @@ class Prediccion:
         
         """Si existe el modelo creado en un archivo lo cargamos.
             Si no, creamos el modelo y lo guardamos en un fichero."""
-        if os.path.isfile('../modelos/modelo_temp.p'):
+        if os.path.isfile('../modelos/temperatura.zip'):
+            """Extraemos el modelo del fichero comprimido."""
+            with zipfile.ZipFile('../modelos/temperatura.zip', 'r') as zipObj:
+               zipObj.extractall("../")
+            """Cargamos el objeto del modelo desde el fichero"""
             arima_temp = pickle.load( open( '../modelos/modelo_temp.p', "rb" ) )
         else:
             arima_temp = pm.auto_arima(dataframe['TEMP'].dropna(), start_p=1, 
                start_q=1, test='adf', max_p=3, max_q=3, m=1, d=None, seasonal=False,
                start_P=0, D=0, trace=True, error_action='ignore', suppress_warnings=True, stepwise=True)
+            """Guardamos el modelo como objeto en un fichero"""
             pickle.dump(arima_temp, open( "../modelos/modelo_temp.p", "wb" ) )
+            """Lo comprimimos para luego subirlo a GitHub"""
+            zipObj = zipfile.ZipFile('../modelos/temperatura.zip', 'w', zipfile.ZIP_DEFLATED)
+            zipObj.write("../modelos/modelo_temp.p")
+            zipObj.close()
  
         """Obtenemos las predicciones de temperatura"""
         predicc_temp, confint = arima_temp.predict(n_periods=tiempo, return_conf_int=True)
 
         """Predicciones de humedad"""
-        if os.path.isfile('../modelos/modelo_hum.p'):
+        if os.path.isfile('../modelos/humedad.zip'):
+            """Extraemos el modelo del fichero comprimido."""
+            with zipfile.ZipFile('../modelos/humedad.zip', 'r') as zipObj:
+               zipObj.extractall("../")
+            """Cargamos el objeto del modelo desde el fichero"""
             arima_hum = pickle.load( open( '../modelos/modelo_hum.p', "rb" ) )
         else:
             arima_hum = pm.auto_arima(dataframe['HUM'].dropna(), start_p=1, 
               start_q=1, test='adf', max_p=3, max_q=3, m=1, d=None, seasonal=False,
               start_P=0, D=0, trace=True, error_action='ignore', suppress_warnings=True, stepwise=True)
+            """Guardamos el modelo como objeto en un fichero"""
             pickle.dump(arima_hum, open( "../modelos/modelo_hum.p", "wb" ) )
+            """Lo comprimimos para luego subirlo a GitHub"""
+            zipObj = zipfile.ZipFile('../modelos/humedad.zip', 'w', zipfile.ZIP_DEFLATED)
+            zipObj.write("../modelos/modelo_hum.p")
+            zipObj.close()
         
         """Obtenemos las predicciones"""
         predicc_hum, confint = arima_hum.predict(n_periods=tiempo, return_conf_int=True)
@@ -69,6 +88,6 @@ class Prediccion:
         return resultado
 
 
-#if __name__== "__main__":
-#    predicc = Prediccion()
-#    print("\nResultado\n",predicc.get_predicciones_arima('hola'))
+if __name__== "__main__":
+    predicc = Prediccion()
+    print("\nResultado\n",predicc.get_predicciones_arima(24))
