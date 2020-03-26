6d5b56f4d8bf0ac1273e6f1afb1e068c50df4901
Lidia Sánchez Mérida <lidia.96.sm@gmail.com>
2020-03-26 11:47:22 +0100

    Actualizado el fichero que captura los datos

--- src/datos.py
+++ src/datos.py
@@ -14,23 +14,23 @@ import pymongo
 
 class Datos:
     
-    def get_datos():
-        # Leemos los dos ficheros csv
+    def get_datos(self):
+        """Leemos los ficheros CSV"""
         df_humedad = pandas.read_csv('/tmp/workflow/humidity.csv')
         df_temperatura = pandas.read_csv('/tmp/workflow/temperature.csv')
-        # Extraemos la columna de la ciudad de San Francisco de humedad y temperatura
+        """Extraemos la columna de la ciudad de San Francisco de humedad y temperatura"""
         humedad_sf = df_humedad['San Francisco']
         temperatura_sf = df_temperatura['San Francisco']
-        # Obtenemos la columna de las fechas porque es la misma en ambos datasets
+        """Obtenemos la columna de las fechas porque es la misma en ambos datasets"""
         datetime = df_humedad['datetime']
-        # Formamos el nuevo dataframe unificado
+        """Formamos el nuevo dataframe unificado"""
         d = {'DATE':datetime, 'TEMP':temperatura_sf, 'HUM':humedad_sf}
         dataframe = pandas.DataFrame(data=d)
-        # Conectamos con el contenedor de MongoDB
+        """Conectamos con el contenedor de MongoDB"""
         cliente = pymongo.MongoClient('mongodb://localhost:27017')
-        # Creamos la base de datos y la colección
+        """Creamos la base de datos y la colección"""
         coleccion = cliente['PrediccionesBD']['Datos']
-        # Transformamos el dataframe a diccionario para poder insertarlo
+        """Transformamos el dataframe a diccionario para poder insertarlo"""
         df_dict = dataframe.to_dict("records")
         indice = coleccion.insert_one({'index':'SF', 'datos':df_dict}).inserted_id
         return indice
