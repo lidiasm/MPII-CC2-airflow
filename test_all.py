76c741af2f6da87161b741c134c95fa5fc43d502
Lidia Sánchez Mérida <lidiasm96@correo.ugr.es>
2020-03-29 18:39:22 +0200

    Tests para la segunda versión

--- test_all.py
+++ test_all.py
@@ -6,7 +6,6 @@ de negocio así como el de los microservicios.
 
 @author: Lidia Sánchez Mérida
 """
-import pandas
 import pytest
 
 """Clase con la lógica de la aplicación para realizar las predicciones"""
@@ -23,7 +22,7 @@ def test2_get_predicciones_arima():
     """Test para comprobar que el objeto devuelto es un dataframe con las predicciones
         realizadas por ARIMA."""
     respuesta = pred.get_predicciones_arima(24)
-    assert type(respuesta) == pandas.core.frame.DataFrame
+    assert type(respuesta) == list
 
 def test1_get_predicciones_api():
     """Test para comprobar que el período indicado es un número. En este caso
@@ -35,7 +34,7 @@ def test2_get_predicciones_api():
     """Test para comprobar que el objeto devuelto es un dataframe con las predicciones
         realizadas por la API Dark Sky."""
     respuesta = pred.get_predicciones_arima(12)
-    assert type(respuesta) == pandas.core.frame.DataFrame
+    assert type(respuesta) == list
 
 """Microservicio de la versión 1: ARIMA"""
 import api_v1
@@ -43,7 +42,7 @@ app_v1 = api_v1.app.test_client()
 
 def test_obtener_predicciones_arima():
     """Test para comprobar el funcionamiento del microservicio de la versión 1."""
-    respuesta = app_v1.get('/arima/24')
+    respuesta = app_v1.get('/arima/12')
     assert (respuesta.status_code == 200)
 
 """Microservicio de la versión 2: Dark Sky API"""
@@ -54,6 +53,6 @@ def test_obtener_predicciones_api():
     """Test para comprobar el funcionamiento del microservicio de la versión 2."""
     respuesta = app_v2.get('/api/12')
     assert (respuesta.status_code == 200)
-    
+
 if __name__ == '__main__':
     pytest.main()
